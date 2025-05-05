5f5d3d30b2634000a7301ea5e91e4dcc9c86ba75
FatRodzianko <jmurphy.5277@gmail.com>
2025-05-04 22:42:06 -0500

    bombrun: adding body parts to health system

--- Assets/BombRunStuff/Scripts/UnitScripts/BombRunUnitHealthSystem.cs
+++ Assets/BombRunStuff/Scripts/UnitScripts/BombRunUnitHealthSystem.cs
@@ -1,10 +1,30 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BodyPartFrozenState
+{
+    None,
+    HalfFrozen,
+    FullFrozen
+}
+public enum BodyPart
+{
+    Head,
+    Arms,
+    Legs
+}
+[Serializable]
+public class BombRunUnitBodyPart
+{
+    public BodyPart BodyPart;
+    public BodyPartFrozenState BodyPartFrozenState;
+}
 public class BombRunUnitHealthSystem : MonoBehaviour
 {
+
     // events
     public event EventHandler OnDead;
     public event EventHandler OnTakeDamage;
@@ -13,6 +33,10 @@ public class BombRunUnitHealthSystem : MonoBehaviour
     [SerializeField] private int _health = 100;
     [SerializeField] private int _maxHealth;
 
+    [Header("Body Parts: Frozen State")]
+    [SerializeField] private List<BombRunUnitBodyPart> _bodyParts = new List<BombRunUnitBodyPart>();
+    private bool _fullyFrozen = false;
+
     private void Awake()
     {
         _health = _startingHealth;
@@ -20,7 +44,13 @@ public class BombRunUnitHealthSystem : MonoBehaviour
     }
     public void TakeDamage(int damageAmount)
     {
-        _health -= damageAmount;
+        //_health -= damageAmount;
+
+        if (_fullyFrozen)
+            TestUnFreezeBodyPart();
+        else
+            TestFreezeBodyPart();
+
         OnTakeDamage?.Invoke(this, EventArgs.Empty);
 
         if (_health <= 0)
@@ -44,5 +74,116 @@ public class BombRunUnitHealthSystem : MonoBehaviour
     public float GetHealthPercentRemaining()
     {
         return (float)_health / _maxHealth;
+    }
+    public List<BombRunUnitBodyPart> GetAllBodyParts()
+    {
+        return _bodyParts;
+    }
+    public BombRunUnitBodyPart GetSpecificBodyPart(BodyPart bodyPart)
+    {
+        if (_bodyParts.Exists(x => x.BodyPart == bodyPart))
+        {
+            return _bodyParts.First(x => x.BodyPart == bodyPart);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public BodyPartFrozenState GetBodyPartFrozenState(BodyPart bodyPart)
+    {
+        if (_bodyParts.Exists(x => x.BodyPart == bodyPart))
+        {
+            BombRunUnitBodyPart newBodyPart = _bodyParts.First(x => x.BodyPart == bodyPart);
+            return newBodyPart.BodyPartFrozenState;
+        }
+        else
+        {
+            return BodyPartFrozenState.None;
+        }
+    }
+    public void TestFreezeBodyPart()
+    {
+        if (GetSpecificBodyPart(BodyPart.Legs).BodyPartFrozenState != BodyPartFrozenState.FullFrozen)
+        {
+            FreezeBodyPart(BodyPart.Legs);
+        }
+        else if (GetSpecificBodyPart(BodyPart.Arms).BodyPartFrozenState != BodyPartFrozenState.FullFrozen)
+        {
+            FreezeBodyPart(BodyPart.Arms);
+        }
+        else if (GetSpecificBodyPart(BodyPart.Head).BodyPartFrozenState != BodyPartFrozenState.FullFrozen)
+        {
+            FreezeBodyPart(BodyPart.Head);
+        }
+        else
+        {
+            _fullyFrozen = true;
+            TestUnFreezeBodyPart();
+        }
+    }
+    public void TestUnFreezeBodyPart()
+    {
+        if (GetSpecificBodyPart(BodyPart.Legs).BodyPartFrozenState != BodyPartFrozenState.None)
+        {
+            UnFreezeBodyPart(BodyPart.Legs);
+        }
+        else if (GetSpecificBodyPart(BodyPart.Arms).BodyPartFrozenState != BodyPartFrozenState.None)
+        {
+            UnFreezeBodyPart(BodyPart.Arms);
+        }
+        else if (GetSpecificBodyPart(BodyPart.Head).BodyPartFrozenState != BodyPartFrozenState.None)
+        {
+            UnFreezeBodyPart(BodyPart.Head);
+        }
+        else
+        {
+            _fullyFrozen = false;
+            TestFreezeBodyPart();
+        }
+    }
+    public void FreezeBodyPart(BodyPart bodyPart)
+    {
+        Debug.Log("FreezeBodyPart: " + bodyPart.ToString());
+        if (!_bodyParts.Exists(x => x.BodyPart == bodyPart))
+        {
+            Debug.Log("FreezeBodyPart: could not find body part: " + bodyPart.ToString());
+        }
+        
+        BombRunUnitBodyPart bombRunUnitBodyPart = _bodyParts.First(x => x.BodyPart == bodyPart);
+
+        switch (bombRunUnitBodyPart.BodyPartFrozenState)
+        {
+            case BodyPartFrozenState.None:
+                bombRunUnitBodyPart.BodyPartFrozenState = BodyPartFrozenState.HalfFrozen;
+                break;
+            case BodyPartFrozenState.HalfFrozen:
+                bombRunUnitBodyPart.BodyPartFrozenState = BodyPartFrozenState.FullFrozen;
+                break;
+            case BodyPartFrozenState.FullFrozen:
+                break;
+        }
+    }
+    public void UnFreezeBodyPart(BodyPart bodyPart)
+    {
+        Debug.Log("UnFreezeBodyPart: " + bodyPart.ToString());
+        if (!_bodyParts.Exists(x => x.BodyPart == bodyPart))
+        {
+            Debug.Log("UnFreezeBodyPart: could not find body part: " + bodyPart.ToString());
+        }
+
+        BombRunUnitBodyPart bombRunUnitBodyPart = _bodyParts.First(x => x.BodyPart == bodyPart);
+
+        switch (bombRunUnitBodyPart.BodyPartFrozenState)
+        {
+            case BodyPartFrozenState.None:
+                break;
+            case BodyPartFrozenState.HalfFrozen:
+                bombRunUnitBodyPart.BodyPartFrozenState = BodyPartFrozenState.None;
+                break;
+            case BodyPartFrozenState.FullFrozen:
+                bombRunUnitBodyPart.BodyPartFrozenState = BodyPartFrozenState.HalfFrozen;
+                break;
+        }
     }
 }
